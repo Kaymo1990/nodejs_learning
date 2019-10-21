e3397ed109cdc347f9ecbb40a925861c9d317de7
Kay <khurrammohammed1990@gmail.com>
2019-10-21 20:30:48 +0100

    testing using chalk

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const theValue = require('./notes/transfer').default
-const validate = require ('validator')
+const theValue = require('./notes/transfer');
+const validate = require ('validator');
+const chalk = require('chalk');
 function hello(name) {
     console.log(`hello ${name}`);
 }
 
 console.log(validate.isURL('khansham@hotmail.com'));
+
+console.log(chalk.blue("hello world"));
